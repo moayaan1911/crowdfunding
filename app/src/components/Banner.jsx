04ec7e4810f55f5a9125199f0d7bc8d91acfd081
moayaan1911 <ayaangames@gmail.com>
2023-08-06 05:58:08 +0530

    modal completed

--- app/src/components/Banner.jsx
+++ app/src/components/Banner.jsx
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import banner from "../assets/charity.jpeg";
+import Modal from "./Modal";
 export default function Banner() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="flex items-center justify-center flex-wrap-reverse w-auto">
       <p className="mx-7 md:w-80 text-center font-sans italic font-bold text-3xl pt-14 w-1/2">
         Contribute to various{" "}
         <span className="text-pink-500 font-mono">CrowdFunding</span> campaigns,
         secured by{" "}
-        <a
-          href="https://polygon.technology/polygon-zkevm"
-          className="underline text-purple-600 font-thin"
-          target="_blank"
+        <p
+          className="underline text-purple-600 font-thin cursor-pointer"
+          onClick={() => setShowModal(true)}
         >
           Polygon zkEVM
-        </a>
+        </p>
       </p>
+      {showModal && <Modal setShowModal={setShowModal} />}
       <img
         src={banner}
-        className="md:w-96 mt-20 border-pink-500 border-8 w-3/5 animate-bounce"
+        className="md:w-96 mt-20 border-pink-500 border-8 w-3/5 animate-pulse"
       />
     </div>
   );
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -4,43 +4,21 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 import App from "./App.jsx";
 import "./index.css";
 const activeChain = {
-  // === Required information for connecting to the network === \\
-  chainId: 1442, // Chain ID of the network
-  // Array of RPC URLs to use
+  chainId: 1442,
   rpc: ["https://polygon-zkevm-testnet.rpc.thirdweb.com"],
 
-  // === Information for adding the network to your wallet (how it will appear for first time users) === \\
-  // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
   nativeCurrency: {
     decimals: 18,
     name: "Ether",
     symbol: "ETH",
   },
-  shortName: "zkEVM testnet", // Display value shown in the wallet UI
-  slug: "zkevmtest", // Display value shown in the wallet UI
-  testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
-  chain: "Polygon", // Name of the network
-  name: "Polygon zkEVM Testnet", // Name of the network
+  shortName: "zkEVM testnet",
+  slug: "zkevmtest",
+  testnet: true,
+  chain: "Polygon",
+  name: "Polygon zkEVM Testnet",
 };
-/*
-ACTIVE CHAINS NAME for Thirdweb
-Ethereum: "ethereum"
-Goerli: "goerli"
-Polygon: "polygon"
-Mumbai: "mumbai"
-Arbitrum One: "arbitrum"
-Arbitrum Goerli: "arbitrum-goerli"
-Optimism: "optimism"
-Optimism Goerli Testnet: "optimism-goerli"
-Binance SmartChain: "binance"
-Binance SmartChain Testnet: "binance-testnet"
-Fantom Opera: "fantom"
-Fantom Testnet: "fantom-testnet"
-Avalanche C Chain: "avalanche-fuji"
-Avalanche Fuji Testnet: "avalanche-fuji-testnet"
-Localhost: "localhost"
-*/
-const clientId = "09c4e70f62deeb54f83478b51e5839ac";
+const clientId = process.env.REACT_APP_THIRDWEB_CLIENT_ID;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThirdwebProvider
