e3b4ef18e30c8faa1d5f2c1b215f807a351d1ddb
moayaan1911 <ayaangames@gmail.com>
2024-05-26 04:26:50 +0530

    removed video

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,37 +1,29 @@
-import { SiEthereum } from "react-icons/si";
+/** @format */
+
+import { SiEthereum } from 'react-icons/si';
 export default function Footer() {
   return (
-    <footer className="bg-pink-500 p-4">
-      <div className="md:flex md:justify-between items-center">
-        <div className="text-black font-bold text-center md:text-left text-2xl mt-2 flex justify-center">
+    <footer className='bg-pink-500 p-4'>
+      <div className='md:flex md:justify-between items-center'>
+        <div className='text-black font-bold text-center md:text-left text-2xl mt-2 flex justify-center'>
           Built by
           <SiEthereum />
           <a
-            className="cursor-pointer underline"
-            target="_blank"
-            rel="noreferrer"
-            href="https://linktr.ee/ayaaneth"
-          >
+            className='cursor-pointer underline'
+            target='_blank'
+            rel='noreferrer'
+            href='https://linktr.ee/ayaaneth'>
             moayaan.eth
           </a>
           <SiEthereum />
         </div>
 
-        <div className="mt-4 md:mt-0 flex justify-center">
+        <div className='mt-4 md:mt-0 flex justify-center'>
           <a
-            className=" text-white px-4 py-2 mr-2 cursor-pointer"
-            target="_blank"
-            rel="noreferrer"
-            href="https://youtu.be/jGEStOsKON0"
-          >
-            Video Demo
-          </a>
-          <a
-            className=" text-white px-4 py-2 cursor-pointer"
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/moayaan1911/crowdfunding"
-          >
+            className=' text-white px-4 py-2 cursor-pointer'
+            target='_blank'
+            rel='noreferrer'
+            href='https://github.com/moayaan1911/crowdfunding'>
             GitHub
           </a>
         </div>
